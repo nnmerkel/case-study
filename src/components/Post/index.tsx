3a2e295eae6a8a5212ddfb64f63cefc8669b00c4
evan merkel <emerkel.dev@gmail.com>
2024-08-28 10:49:15 -0700

    add relative date formatting

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import { Image, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { formatDistance, formatRelative } from 'date-fns';
 
 import { ThemedView } from '../ThemedView';
 import { ThemedText } from '../ThemedText';
 import IconButton from '../IconButton';
 import type { IRedditPost } from './Post';
 
-const fetchImage = (url: string): Promise<string> => {
-	return fetch(url)
-		.then((res) => res.blob())
-		.then((blob) => {
-			return Promise.resolve(URL.createObjectURL(blob));
-		})
-		.catch((e) => {
-			console.error(e);
-			return Promise.reject();
-		});
-};
+// const fetchImage = (url: string): Promise<string> => {
+// 	return fetch(url)
+// 		.then((res) => res.blob())
+// 		.then((blob) => {
+// 			return Promise.resolve(URL.createObjectURL(blob));
+// 		})
+// 		.catch((e) => {
+// 			console.error(e);
+// 			return Promise.reject();
+// 		});
+// };
 
 const formatDate = (input: number): string => {
 	// reddit post `created` and `created_utc` UNIX timestamps are in seconds so we need to multiply by 1000 for js dates
-	return new Date(input * 1000).toLocaleString();
+	return formatDistance(new Date(input * 1000), new Date(), {});
 };
 
 function Post({
-	id,
 	author,
 	created,
 	num_comments,
@@ -35,37 +34,30 @@ function Post({
 }: IRedditPost) {
 	return (
 		<ThemedView style={styles.wrapper}>
-			<Link href={`/post/${id}`}>
-				<ThemedView style={styles.row}>
-					<ThemedText type="small" style={styles.author}>
-						u/{author}
-					</ThemedText>
-					<ThemedText type="small">&bull;</ThemedText>
-					<ThemedText type="small" style={styles.created}>
-						{`${formatDate(created)}`}
-					</ThemedText>
-				</ThemedView>
-				<ThemedView style={styles.header}>
-					<ThemedText type="subtitle">{title}</ThemedText>
-				</ThemedView>
-				<Image source={{ uri: thumbnail }} />
+			<ThemedView style={styles.row}>
+				<ThemedText type="small" style={styles.author}>
+					u/{author}
+				</ThemedText>
+				<ThemedText type="small">&bull;</ThemedText>
+				<ThemedText type="small" style={styles.created}>
+					{`${formatDate(created)} ago`}
+				</ThemedText>
+			</ThemedView>
+			<ThemedView style={styles.header}>
+				<ThemedText type="subtitle">{title}</ThemedText>
+			</ThemedView>
+			<ThemedView style={styles.imgWrapper}>
 				<Image
 					accessible={false}
 					source={{ uri: thumbnail }}
-					// style={styles.thumbnail}
+					style={styles.thumbnail}
 					resizeMode="contain"
-					width={140}
-					height={140}
-					// onLoad={({ nativeEvent }) => {
-					// 	console.log(nativeEvent, nativeEvent.source);
-					// 	// setDims({ width, height });
-					// }}
 				/>
-				<ThemedView style={styles.row}>
-					<IconButton name="arrow-up">{score}</IconButton>
-					<IconButton name="comments-o">{num_comments}</IconButton>
-				</ThemedView>
-			</Link>
+			</ThemedView>
+			<ThemedView style={styles.row}>
+				<IconButton name="arrow-up">{score}</IconButton>
+				<IconButton name="comments-o">{num_comments}</IconButton>
+			</ThemedView>
 		</ThemedView>
 	);
 }
@@ -90,9 +82,11 @@ const styles = StyleSheet.create({
 		marginTop: 8,
 		marginBottom: 8,
 	},
-	author: {
-		// opacity: 0.5,
+	imgWrapper: {
+		width: '100%',
+		marginBottom: 12,
 	},
+	author: {},
 	created: {
 		opacity: 0.5,
 	},
